4742f601064edfcf085db5b45f5135df660f2041
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2022-07-09 11:35:43 -0500

    some cleanup and formatting...

--- M17RX.h
+++ M17RX.h
@@ -17,38 +17,43 @@
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#if !defined(M17RX_H)
-#define  M17RX_H
+#if !defined(M17RX_H_)
+#define M17RX_H_
 
 #include "M17Defines.h"
 
+
+
 enum M17RX_STATE {
-  M17RXS_NONE,
-  M17RXS_LINK_SETUP,
-  M17RXS_STREAM
+	M17RXS_NONE,
+	M17RXS_LINK_SETUP,
+	M17RXS_STREAM
 };
+
+
 
 class CM17RX {
 public:
-  CM17RX();
+	CM17RX();
 
-  void databit(bool bit);
+	void databit(bool bit);
 
-  void reset();
+	void reset();
 
 private:
-  M17RX_STATE m_state;
-  uint16_t    m_bitBuffer;
-  uint8_t     m_outBuffer[M17_FRAME_LENGTH_BYTES + 3U];
-  uint8_t*    m_buffer;
-  uint16_t    m_bufferPtr;
-  uint16_t    m_lostCount;
+	M17RX_STATE m_state;
+	uint16_t m_bitBuffer;
+	uint8_t m_outBuffer[M17_FRAME_LENGTH_BYTES + 3U];
+	uint8_t *m_buffer;
+	uint16_t m_bufferPtr;
+	uint16_t m_lostCount;
 
-  void processNone(bool bit);
-  void processData(bool bit);
-  void writeRSSILinkSetup(uint8_t* data);
-  void writeRSSIStream(uint8_t* data);
+	void processNone(bool bit);
+	void processData(bool bit);
+	void writeRSSILinkSetup(uint8_t *data);
+	void writeRSSIStream(uint8_t *data);
 };
 
-#endif
 
+
+#endif	/* !M17RX_H_ */
--- M17TX.cpp
+++ M17TX.cpp
@@ -23,102 +23,102 @@
 
 #include "M17Defines.h"
 
+
+
 const uint8_t M17_SYNC = 0x77U;
 const uint8_t M17_PREAMBLE = 0x77U;
 
-CM17TX::CM17TX() :
-m_buffer(1500U),
-m_poBuffer(),
-m_poLen(0U),
-m_poPtr(0U),
-m_txDelay(240U),      // 200ms
-m_delay(false)
-{
+
+
+CM17TX::CM17TX(): m_buffer(1500U),
+		m_poBuffer(),
+		m_poLen(0U),
+		m_poPtr(0U),
+		m_txDelay(240U),      // 200ms
+		m_delay(false) {
 }
 
-void CM17TX::process()
-{
-  if (m_buffer.getData() == 0U && m_poLen == 0U)
-    return;
+void CM17TX::process() {
+	if (m_buffer.getData() == 0U && m_poLen == 0U)
+		return;
 
-  if (m_poLen == 0U) {
-    if (!m_tx) {
-      m_delay = true;
-      m_poLen = m_txDelay;
-    } else {
-      m_delay = false;
-      for (uint8_t i = 0U; i < M17_FRAME_LENGTH_BYTES; i++)
-        m_poBuffer[m_poLen++] = m_buffer.get();
-    }
+	if (m_poLen == 0U) {
+		if (!m_tx) {
+			m_delay = true;
+			m_poLen = m_txDelay;
+		} else {
+			m_delay = false;
+			for (uint8_t i = 0U; i < M17_FRAME_LENGTH_BYTES; i++)
+				m_poBuffer[m_poLen++] = m_buffer.get();
+		}
 
-    m_poPtr = 0U;
-  }
+		m_poPtr = 0U;
+	}
 
-  if (m_poLen > 0U) {
-    uint16_t space = io.getSpace();
-    
-    while (space > 8U) {
-      if (m_delay) {
-        writeByte(M17_SYNC);
-        m_poPtr++;
-      } else
-        writeByte(m_poBuffer[m_poPtr++]); 
+	if (m_poLen > 0U) {
+		uint16_t space = io.getSpace();
 
-      space -= 8U;
-      
-      if (m_poPtr >= m_poLen) {
-        if (m_delay) {
-          m_delay = false;
-          m_poPtr = 0U;
-          m_poLen = 3U;
-        } else {
-          m_poPtr = 0U;
-          m_poLen = 0U;
-          m_delay = false;
-          return;
-        }
-      }
-    }
-  }
+		while (space > 8U) {
+			if (m_delay) {
+				writeByte(M17_SYNC);
+				m_poPtr++;
+			} else
+				writeByte(m_poBuffer[m_poPtr++]);
+
+			space -= 8U;
+
+			if (m_poPtr >= m_poLen) {
+				if (m_delay) {
+					m_delay = false;
+					m_poPtr = 0U;
+					m_poLen = 3U;
+				} else {
+					m_poPtr = 0U;
+					m_poLen = 0U;
+					m_delay = false;
+					return;
+				}
+			}
+		}
+	}
 }
 
 uint8_t CM17TX::writeData(const uint8_t* data, uint8_t length)
 {
-  if (length != (M17_FRAME_LENGTH_BYTES + 1U))
-    return 4U;
+	if (length != (M17_FRAME_LENGTH_BYTES + 1U))
+		return 4U;
 
-  uint16_t space = m_buffer.getSpace();
-  if (space < M17_FRAME_LENGTH_BYTES)
-    return 5U;
+	uint16_t space = m_buffer.getSpace();
+	if (space < M17_FRAME_LENGTH_BYTES)
+		return 5U;
 
-  for (uint8_t i = 0U; i < M17_FRAME_LENGTH_BYTES; i++)
-    m_buffer.put(data[i + 1U]);
+	for (uint8_t i = 0U; i < M17_FRAME_LENGTH_BYTES; i++)
+		m_buffer.put(data[i + 1U]);
 
-  return 0U;
+	return 0U;
 }
 
 void CM17TX::writeByte(uint8_t c)
 {
-  uint8_t bit;
-  uint8_t mask = 0x80U;
-  
-  for (uint8_t i = 0U; i < 8U; i++, c <<= 1) {
-    if ((c & mask) == mask)
-      bit = 1U;
-    else
-      bit = 0U;
+	uint8_t bit;
+	uint8_t mask = 0x80U;
 
-    io.write(&bit, 1);
-  }
+	for (uint8_t i = 0U; i < 8U; i++, c <<= 1) {
+		if ((c & mask) == mask)
+			bit = 1U;
+		else
+			bit = 0U;
+
+		io.write(&bit, 1);
+	}
 }
 
 void CM17TX::setTXDelay(uint8_t delay)
 {
-  m_txDelay = 600U + uint16_t(delay) * 12U;        // 500ms + tx delay
+	m_txDelay = 600U + uint16_t(delay) * 12U;        // 500ms + tx delay
 }
 
 uint8_t CM17TX::getSpace() const
 {
-  return m_buffer.getSpace() / M17_FRAME_LENGTH_BYTES;
+	return m_buffer.getSpace() / M17_FRAME_LENGTH_BYTES;
 }
-
